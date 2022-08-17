02f4478a8b4d716365790adf79a64b7ca18bb7c0
Matthew Perrot <Matthew@Matthews-MacBook-Air.local>
2022-08-16 22:09:33 -0700

    added images

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Milestone-Project-1
+
+-Add Images
+-Add animations
+-Show win/lose on screen
+-Stop at zero
+
+-maybe add sounds?
--- scripts.js
+++ scripts.js
@@ -1,14 +1,14 @@
  let user = {
-    funds: 1000
+    funds: 2000
 }
 
-let slotMachine = ["cherry", "seven", "bell"]
+let slotMachine = ["cherries", "seven", "bell"]
 let window1 = document.getElementById("window1")
 let window2 = document.getElementById("window2")
 let window3 = document.getElementById("window3")
 
 function spin() {
-    user.funds -= 50;
+    user.funds -= 150;
     window1.textContent = slotMachine[Math.floor(Math.random() * 3)];
     window2.textContent = slotMachine[Math.floor(Math.random() * 3)];
     window3.textContent = slotMachine[Math.floor(Math.random() * 3)];  
@@ -21,9 +21,10 @@ function checkForWin() {
     if(
         window1.textContent === window2.textContent && window3.textContent === window1.textContent
     ){
-        console.log("You win!")
-        user.funds += 250
+        console.log("You win!");
+        user.funds += 350;
+        document.getElementById("user-funds").textContent = user.funds;
     } else {
-        console.log("Try again!")
+        console.log("Try again!");
     }
 }
