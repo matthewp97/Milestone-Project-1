a754495acd8abfd61865aa1004c4e65bc72481ca
Matthew Perrot <Matthew@Matthews-MacBook-Air.local>
2022-08-23 18:07:44 -0700

    commit

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # Milestone-Project-1 Ideas
 
+-Change cherry
+-Separate word "Credits" from credit amount
+-Add click effect to sping button
 -Add animations
--Border moves when win/lose pop up
--Make add credit function/button
+    -Animate images
+    -Animate text when adding more credits
+-Find good placement for win/lose notification
 -Certain combos win more
 
 -maybe add sounds?
--- scripts.js
+++ scripts.js
@@ -19,7 +19,7 @@ function spin() {
     window2.src = slotMachine[Math.floor(Math.random() * 3)];
     window3.src = slotMachine[Math.floor(Math.random() * 3)];  
     document.getElementById("user-funds").textContent = "Credits: " + user.funds;
-    checkForWin()
+    checkForWin();
     console.log(user.funds);
     lowFunds();
 }
@@ -28,13 +28,13 @@ function checkForWin() {
     if(
         window1.src === window2.src && window3.src === window1.src
     ){
-        win.style = "display:flex"
+        win.style = "display:flex";
         console.log("You win!");
         user.funds += 200;
         document.getElementById("user-funds").textContent = "Credits: " + user.funds;
     } else if (user.funds <= 0 ){
-        loss.style = "display:flex"
-        spinButton.disabled = true
+        loss.style = "display:flex";
+        spinButton.disabled = true;
         console.log("Try again!");
         addCredits.style.display = "inline-block";
     }
